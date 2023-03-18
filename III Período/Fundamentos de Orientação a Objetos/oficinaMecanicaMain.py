95c477c5e365333a28472402f2a843984a6d878e
Miguel Avelar <88055057+MiguelMA3@users.noreply.github.com>
2023-03-17 21:45:05 -0300

    Update main.py

---
+++ III Período/Fundamentos de Orientação a Objetos/oficinaMecanicaMain.py
@@ -0,0 +1,20 @@
+
+class Pessoa:
+  __init__(self, nome, cpf, sexo):
+    nome = __self.nome;
+    cpf = __self.cpf;
+    sexo = __self.sexo;
+
+class Profissional(Pessoa):
+  __init__(self):
+    pass;
+  
+  def orcamento():
+    pass;
+  
+class Cliente(Pessoa):
+  __init__(self):
+    pass;
+  
+  def pagar():
+    pass;
