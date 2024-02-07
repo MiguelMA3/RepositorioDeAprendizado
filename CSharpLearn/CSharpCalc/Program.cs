d7523e8a5fe95b9bfc83eb40c0537a017bc093fd
Miguel Maletzke <88055057+MiguelMA3@users.noreply.github.com>
2024-02-07 11:32:12 -0300

    Add files via upload

---
+++ CSharpLearn/CSharpCalc/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result = 0;
+            string answer;
+
+            Console.WriteLine("Welcome!");
+
+            Console.WriteLine("Enter a Number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another Number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What type of operation would you like to do?");
+            Console.WriteLine("a - addition\ns - subtraction\nm - multiplication\nd - division");
+
+            answer = Console.ReadLine();
+
+            switch (answer)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option!");
+                    break;
+            }
+
+            //Console.WriteLine("Result: " + result);
+            Console.WriteLine("$Result: {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpLearn/DiceGame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DiceGame {
+    class Program {
+            static void Main(string[] args) {
+                int playerRandomNum, enemyRandomNum;
+                int playerPoints = 0, enemyPoints = 0;
+
+                Random random = new Random();
+
+                for (int i = 0; i < 10; i++) {
+                    Console.WriteLine("\n= Press any key to roll the dice =");
+                    Console.ReadKey();
+                    
+                    playerRandomNum = random.Next(1, 7);
+                    Console.WriteLine($"You rolled a {playerRandomNum}");
+                    playerPoints += playerRandomNum;
+                    Console.WriteLine("...");
+                    System.Threading.Thread.Sleep(1000);
+
+                    enemyRandomNum = random.Next(1, 7);
+                    Console.WriteLine($"Enemy AI rolled a {enemyRandomNum}");
+                    enemyPoints += enemyRandomNum;
+                }
+
+                Console.WriteLine("\n===Points===");
+                Console.WriteLine($"Player: {playerPoints}\nEnemy: {enemyPoints}");
+
+                if (playerPoints > enemyPoints) {
+                    Console.WriteLine("You WON the Game!");
+                } else if (playerPoints < enemyPoints) {
+                    Console.WriteLine("You LOSE the Game!");
+                } else {
+                    Console.WriteLine("DRAW!");
+                }
+                Console.ReadKey();
+            }
+    }
+}
