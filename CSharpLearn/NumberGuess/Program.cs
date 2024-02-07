b3ef3c7f53f2a72e22c234c26e07eea8f432a4a1
Miguel Maletzke <88055057+MiguelMA3@users.noreply.github.com>
2024-02-07 14:16:00 -0300

    Add files via upload

---
+++ CSharpLearn/NumberGuess/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace NumberGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCorrectGuess = false;
+            Random random = new Random();
+
+            int randomNum = random.Next(1, 10);
+
+            Console.WriteLine("Number Guessing Game");
+            Console.WriteLine("A number between 1 and 10 will be generated.");
+
+            while (!isCorrectGuess)
+            {
+                Console.WriteLine("Please enter your guess.");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess > randomNum)
+                {
+                    Console.WriteLine("Too high.");
+                }
+                else if (guess < randomNum)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else
+                {
+                    Console.WriteLine("Correct!");
+                    isCorrectGuess = true;
+                }
+            }
+
+            Console.WriteLine("Congratulations, you have won the game!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpLearn/ShapeArea/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ShapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            float result;
+
+            Console.WriteLine("What shape would you lik to find the area of?\nPlease ebter 'r' for rectangle or any other key for circle.");
+            answer = Console.ReadLine();
+
+            if (answer == "r")
+            {
+                Console.WriteLine("Please enter the height of the rectangle.");
+                float height = float.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter the width of the rectangle.");
+                float width = float.Parse(Console.ReadLine());
+
+                result = height * width;
+            }
+            else
+            {
+                Console.WriteLine("Please enter the radius of the circle.");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius * radius);
+            }
+
+            Console.WriteLine($"Result: {result}");
+            Console.ReadKey();
+        }
+    }
+}
